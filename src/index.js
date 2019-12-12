922d6f6b31d0909bb4a046e7975d4fff311652c7
AntonioHamilton <antoniohamiltonfreitas@outlook.com>
2019-12-12 12:07:28 -0300

    try to adjust cors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(cors());
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -13,8 +15,6 @@ BDconnection;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
-
-app.use(cors());
 
 app.use ((req, res, next) => {
     req.io = io;
